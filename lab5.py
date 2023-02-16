30c3493dac4b325d4ba5df7fd19e32a177f3ad6a
EC2 Default User <ec2-user@ip-172-31-37-91.ec2.internal>
2023-02-16 22:02:50 +0000

    lab5

---
+++ lab5.py
@@ -0,0 +1,42 @@
+'''
+lab 5
+
+if statement
+'''
+
+#3.1
+a_color = 'green'
+
+if a_color == 'green': 
+    print("you've won 5 points")
+    
+#3.2
+a_color = 'pink'
+
+if a_color == 'green': 
+    print("you've won 5 points")
+else:
+     print("you've won 10 points")
+     
+#3.3
+favorite_fruits =['mango', 'banana', 'strawberries']
+
+if 'mango'in favorite_fruits:
+    print('i really like the flavor of a mango')
+if 'banana' in favorite_fruits:
+    print('bananas are my favorite to use for smoothies')
+if ' strawberries'in favorite_fruits:
+    print("i love chocolate covered strawberries")
+
+#3.4
+age = 90
+
+if age < 10:
+    print('kid')
+elif age < 20:
+    print('teenager')
+else:
+    print('adult')
+    if age > 65:
+        print ('elder')
+    
